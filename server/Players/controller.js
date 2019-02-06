92f0913e4e4e23b644923ef1cc2dd1d3a51642fc
Julius Camarillo <julcamarillo17@gmail.com>
2019-02-06 11:43:42 -0600

    get post and put are working

--- server/Players/controller.js
+++ server/Players/controller.js
@@ -23,14 +23,38 @@ client.connect()
 })
 
 router.get('/',(request,response)=>{
-	let query = "SELECT * FROM players WHERE nationality = 'Brazil' "
+	let query = 'SELECT * FROM staff ' 
 	client.query(query)
 	.then(result=>{
-		response.send(result)
+		response.send(result.rows)
 	})
 	.catch(error =>
 		response.send(error))
 	})
+router.post('/',(request,response)=>{
+	let input = 'INSERT into STAFF(staff_id,staff_position,staff_name)VALUES($1,$2,$3)'
+
+	let values = [request.body.staff_id, request.body.staff_position, request.body.staff_name]
+	client.query(input, values)
+	.then(result => response.send(result))
+	.catch(error =>{
+		console.log(error)
+		response.send("OH NOOOO Error")
+	})
+})	
+
+router.put('/:id',(request,rsponse)=>{
+
+	let query = 'UPDATE STAFF SET staff_position=($1),staff_name=($2) WHERE staff_id=($3)'
+	const id = request.params.id
+	let data = {staff_position:request.body.staff_position, staff_name:request.body.staff_name }
+	client.query(query, [data.staff_position, data.staff_name,id])
+	.then(result=> response.send(result))
+	.catch(error=> {
+		console.log(error)
+		response.send("AHHH Error")
+	})
+})
 
 
 module.exports = router
